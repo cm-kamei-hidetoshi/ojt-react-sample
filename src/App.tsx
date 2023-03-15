a346b7ee12e120cda26b8af216c41567ec6c3b8d
Kamei Hidetoshi <kamei.hidetoshi@classmethod.jp>
2023-03-15 11:02:15 +0900

    product api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,43 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import style from "./App.module.css";
 
+type ProductsResponse = {
+  products: Product[];
+};
+
+type Product = {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+};
+
 function App() {
+  const [products, setProducts] = useState<Product[]>();
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((res) => setProducts(res.products));
+  }, []);
+
   return (
     <>
       <header className={style.header}>header</header>
-      <main className={style.main}>main</main>
+      <main className={style.main}>
+        {products?.map((product) => (
+          <p>
+            {product.id}:{product.title}
+          </p>
+        ))}
+      </main>
       <footer className={style.footer}>footer</footer>
     </>
   );
