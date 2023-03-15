c9e2fd8da865350493849db7a970a73c375272ff
Kamei Hidetoshi <kamei.hidetoshi@classmethod.jp>
2023-03-15 10:04:56 +0900

    new project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import style from "./App.module.css";
+
+function App() {
+  return (
+    <>
+      <header className={style.header}>header</header>
+      <main className={style.main}>main</main>
+      <footer className={style.footer}>footer</footer>
+    </>
+  );
+}
+
+export default App;
